3b397cbe9aea0827eeaaece6a3074612d79dea9c
DheenaAmjath <dheenaamjath8@gmail.com>
2026-06-08 14:42:11 +0530

    Add contact API routes

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,15 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import contactRoutes from './routes/contactRoutes.js';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use('/contacts', contactRoutes)
 
 
 
